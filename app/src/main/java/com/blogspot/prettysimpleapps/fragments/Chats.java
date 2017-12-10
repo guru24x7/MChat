258a65202649bb17ce2cf7cd6f863db8795cfd98
Saqib <gaming.guru24x7@gmail.com>
2017-12-10 11:21:27 +0500

    Initial commit

---
+++ app/src/main/java/com/blogspot/prettysimpleapps/fragments/Chats.java
@@ -0,0 +1,81 @@
+package com.blogspot.prettysimpleapps.fragments;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.blogspot.prettysimpleapps.mchat.ChatAdapter;
+import com.blogspot.prettysimpleapps.mchat.ChatMessage;
+import com.blogspot.prettysimpleapps.mchat.CommonMethods;
+import com.blogspot.prettysimpleapps.mchat.R;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Chats extends Fragment implements View.OnClickListener {
+
+    private EditText msg_edittext;
+    private String user1 = "khushi", user2 = "khushi2";
+    private Random random;
+    public static ArrayList<ChatMessage> chatlist;
+    public static ChatAdapter chatAdapter;
+
+    ListView msgListView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.chat_layout, container, false);
+        random = new Random();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Chats");
+        msg_edittext = (EditText) view.findViewById(R.id.messageEditText);
+        msgListView = (ListView) view.findViewById(R.id.msgListView);
+
+        ImageButton sendButton = (ImageButton) view.findViewById(R.id.sendMessageButton);
+
+        sendButton.setOnClickListener(this);
+
+        // set autoscroll of listview when a new message arrives
+        msgListView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        msgListView.setStackFromBottom(true);
+
+        chatlist = new ArrayList<ChatMessage>();
+        chatAdapter = new ChatAdapter(getActivity(), chatlist);
+        msgListView.setAdapter(chatAdapter);
+        return view;
+    }
+
+    public void sendTextMessage(View v) {
+        String message = msg_edittext.getEditableText().toString();
+        if (!message.equalsIgnoreCase("")) {
+            final ChatMessage chatMessage = new ChatMessage(user1, user2,
+                    message, "" + random.nextInt(1000), true);
+            chatMessage.setMsgID();
+            chatMessage.body = message;
+            chatMessage.Date = CommonMethods.getCurrentDate();
+            chatMessage.Time = CommonMethods.getCurrentTime();
+            msg_edittext.setText("");
+            chatAdapter.add(chatMessage);
+            chatAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sendMessageButton:
+                sendTextMessage(v);
+        }
+
+    }
+}
